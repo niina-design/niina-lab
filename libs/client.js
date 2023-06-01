fdf5aee581f2d67823ca527dbcacb70c2c47e729
yangae <cyangae@gmail.com>
2023-06-01 22:46:20 +0900

    apikeyをローカルのenvに置く

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'niina-lab',  // service-domain は XXXX.microcms.io の XXXX 部分
-  apiKey: 'wy57ucXYGpEPjOiFPDhiq5TI3US7xgrL9egY',
+  apiKey: process.env.API_KEY,
 });
